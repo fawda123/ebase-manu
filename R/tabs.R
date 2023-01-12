7ec659dbe90618117dec7106758af7572201e37e
fawda123 <mbafs2012@gmail.com>
2023-01-12 11:53:08 -0500

    model io table done

--- R/tabs.R
+++ R/tabs.R
@@ -5,6 +5,65 @@ library(here)
 
 source(file = here('R/funcs.R'))
 
+# EBASE i/o -----------------------------------------------------------------------------------
+
+totab <- tibble(
+  Type = c('Input', 'Input', 'Input', 'Input', 'Input', 'Input',
+           'Input-derived', 'Input-derived', 'Input-derived',
+           'Output', 'Output', 'Output', 'Output', 'Output', 'Output'),
+  Description = c('Dissolved oxygen', 'Water temperature', 'Salinity', 'Total photosynthetically active radiation', 'Wind speed', 'Water column depth',
+                  'Wind speed at 10 meter height, squared', 'Schmidt number (from water temperature and salinity)', 'Dissolved oxygen at saturation (from water temperature and salinity)',
+                  'Dissolved oxygen (modelled)', 'Production', 'Respiration', 'Gas exchange', 'Light efficiency', 'b'),
+  `Model notation` =  c('C_d', '-', '-', 'PAR', '-', 'H', 
+                        'U102', 'Schmidt number', 'C_sat', 
+                        'C_mod', 'Pg_vol', 'Rt_vol',  'D', 'a', 'b'),
+  Units = c('mg/L', 'C', 'psu', 'W/m2/s', 'm/s', 'm', 
+            'm2', 'unitless', 'mmol/m3',
+            'mmol/m3', 'mmol O2 m3/d', 'mmol O2 m3/d', 'mmol O2 m3/d', '(mmol/m3/d)/(W/m2)', '(cm/hr)/(m2/s2)')
+) %>% 
+  flextable::as_grouped_data(groups = 'Type')
+
+eqsz <- fp_text_default(font.size = 9)
+# col2wid <- 1
+# col3wid <- 2
+# col14wid <- 6.5 - (col2wid + col3wid)
+ebaseiotab <- flextable(totab) %>% 
+  flextable::compose(i = 2, j = 3, value = as_paragraph(as_equation('C_d', props = eqsz))) %>% 
+  flextable::compose(i = 5, j = 3, value = as_paragraph(as_equation('PAR', props = eqsz))) %>% 
+  flextable::compose(i = 7, j = 3, value = as_paragraph(as_equation('H', props = eqsz))) %>% 
+  flextable::compose(i = 9, j = 3, value = as_paragraph(as_equation('U_{10}^2', props = eqsz))) %>% 
+  flextable::compose(i = 10, j = 3, value = as_paragraph(as_equation('S_c', props = eqsz))) %>% 
+  flextable::compose(i = 11, j = 3, value = as_paragraph(as_equation('C_{sat}', props = eqsz))) %>% 
+  flextable::compose(i = 13, j = 3, value = as_paragraph(as_equation('C_{mod}', props = eqsz))) %>% 
+  flextable::compose(i = 14, j = 3, value = as_paragraph(as_equation('Pg_{vol}, aPAR', props = eqsz))) %>% 
+  flextable::compose(i = 15, j = 3, value = as_paragraph(as_equation('Rt_{vol}, r', props = eqsz))) %>% 
+  flextable::compose(i = 16, j = 3, value = as_paragraph(as_equation('D, \\frac{1}{H}\\left[-bU_{10}^2\\left(\\frac{s_c}{600} \\right)^{-0.5} \\left(C_{Sat} - C_d\\right )\\right]', props = eqsz))) %>% 
+  flextable::compose(i = 17, j = 3, value = as_paragraph(as_equation('a', props = eqsz))) %>% 
+  flextable::compose(i = 18, j = 3, value = as_paragraph(as_equation('b', props = eqsz))) %>% 
+  flextable::compose(i = 2, j = 4, value = as_paragraph(as_equation('\\text{mg}/\\text{L}', props = eqsz))) %>% 
+  flextable::compose(i = 3, j = 4, value = as_paragraph(as_equation('\\degree\\text{C}', props = eqsz))) %>% 
+  flextable::compose(i = 4, j = 4, value = as_paragraph(as_equation('\\text{psu}', props = eqsz))) %>% 
+  flextable::compose(i = 5, j = 4, value = as_paragraph(as_equation('\\text{Watts}/\\text{m}^2', props = eqsz))) %>% 
+  flextable::compose(i = 6, j = 4, value = as_paragraph(as_equation('\\text{m}/\\text{s}', props = eqsz))) %>% 
+  flextable::compose(i = 7, j = 4, value = as_paragraph(as_equation('\\text{m}', props = eqsz))) %>% 
+  flextable::compose(i = 9, j = 4, value = as_paragraph(as_equation('\\text{m}^2/\\text{s}^2', props = eqsz))) %>% 
+  flextable::compose(i = 11, j = 4, value = as_paragraph(as_equation('\\text{mmol}/\\text{m}^3', props = eqsz))) %>% 
+  flextable::compose(i = 13, j = 4, value = as_paragraph(as_equation('\\text{mmol}/\\text{m}^3', props = eqsz))) %>% 
+  flextable::compose(i = 14, j = 4, value = as_paragraph(as_equation('\\text{mmol}/\\text{m}^3/\\text{d}', props = eqsz))) %>% 
+  flextable::compose(i = 15, j = 4, value = as_paragraph(as_equation('\\text{mmol}/\\text{m}^3/\\text{d}', props = eqsz))) %>% 
+  flextable::compose(i = 16, j = 4, value = as_paragraph(as_equation('\\text{mmol}/\\text{m}^3/\\text{d}', props = eqsz))) %>% 
+  flextable::compose(i = 17, j = 4, value = as_paragraph(as_equation('\\left(\\text{mmol}/\\text{m}^2/\\text{d}\\right)/\\left(\\text{Watts}/\\text{m}^2\\right)', props = eqsz))) %>% 
+  flextable::compose(i = 18, j = 4, value = as_paragraph(as_equation('\\left(\\text{cm}/\\text{hr}\\right)/\\left(\\text{m}^2/\\text{s}^2\\right)', props = eqsz))) %>%
+  flextable::font(part = 'all', fontname = 'Times New Roman') %>% 
+  flextable::align(align = 'center', j = 3:4, part = 'all') %>% 
+  width(j = 1, width = 0.5) %>% 
+  width(j = 2, width = 2) %>%
+  width(j = 3, width = 2) %>% 
+  width(j = 4, width = 2) %>% 
+  padding(padding = 1, part = 'all')  
+
+save(ebaseiotab, file = here('tabs/ebaseiotab.RData'))
+
 # comparison of EBASE to other methods with actual apa data -----------------------------------
 
 load(file = url('https://github.com/fawda123/BASEmetab_script/raw/master/data/apacmp.RData'))
