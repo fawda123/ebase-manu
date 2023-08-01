794293d285debee2cd5eccd0e9614e375f506e01
fawda123 <mbafs2012@gmail.com>
2023-08-01 16:55:21 -0400

    added info about missing data

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -5,3 +5,8 @@ library(here)
 
 fl <- here('data/apasumdat.RData')
 download.file('https://github.com/fawda123/BASEmetab_script/raw/master/data/apasumdat.RData', destfile = fl)
+
+# misdat, missing data in apacp 2021 ----------------------------------------------------------
+
+fl <- here('data/misdat.RData')
+download.file('https://github.com/fawda123/BASEmetab_script/raw/master/data/misdat.RData', destfile = fl)
