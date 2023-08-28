9ab5ae908d3326f382a77f59c99ac84bc6b73949
fawda123 <mbafs2012@gmail.com>
2023-08-28 09:31:18 -0400

    fixing tables from RN comments

--- R/tabs.R
+++ R/tabs.R
@@ -13,7 +13,7 @@ totab <- tibble(
            'Output', 'Output', 'Output', 'Output', 'Output', 'Output'),
   Description = c('Dissolved oxygen', 'Water temperature', 'Salinity', 'Total photosynthetically active radiation', 'Wind speed', 'Water column depth',
                   'Wind speed at 10 meter height, squared', 'Schmidt number (from water temperature and salinity)', 'Dissolved oxygen at saturation (from water temperature and salinity)',
-                  'Dissolved oxygen (modelled)', 'Production', 'Respiration', 'Gas exchange', 'Light efficiency', 'b'),
+                  'Dissolved oxygen (modelled)', 'Production', 'Respiration', 'Gas exchange', 'Light efficiency', 'Sensitivity of gas transfer to wind speed'),
   `Model notation` =  c('C_d', '-', '-', 'PAR', '-', 'H', 
                         'U102', 'Schmidt number', 'C_sat', 
                         'C_mod', 'P', 'R',  'D', 'a', 'b'),
@@ -28,11 +28,11 @@ eqsz <- fp_text_default(font.size = 9)
 # col3wid <- 2
 # col14wid <- 6.5 - (col2wid + col3wid)
 ebaseiotab <- flextable(totab) %>% 
-  flextable::compose(i = 2, j = 3, value = as_paragraph(as_equation('C_d', props = eqsz))) %>% 
+  flextable::compose(i = 2, j = 3, value = as_paragraph(as_equation('C', props = eqsz))) %>% 
   flextable::compose(i = 5, j = 3, value = as_paragraph(as_equation('PAR', props = eqsz))) %>% 
   flextable::compose(i = 7, j = 3, value = as_paragraph(as_equation('H', props = eqsz))) %>% 
   flextable::compose(i = 9, j = 3, value = as_paragraph(as_equation('U_{10}^2', props = eqsz))) %>% 
-  flextable::compose(i = 10, j = 3, value = as_paragraph(as_equation('S_c', props = eqsz))) %>% 
+  flextable::compose(i = 10, j = 3, value = as_paragraph(as_equation('Sc', props = eqsz))) %>% 
   flextable::compose(i = 11, j = 3, value = as_paragraph(as_equation('C_{sat}', props = eqsz))) %>% 
   flextable::compose(i = 13, j = 3, value = as_paragraph(as_equation('C_{mod}', props = eqsz))) %>% 
   flextable::compose(i = 14, j = 3, value = as_paragraph(as_equation('P,\\,aPAR', props = eqsz))) %>% 
@@ -40,19 +40,19 @@ ebaseiotab <- flextable(totab) %>%
   flextable::compose(i = 16, j = 3, value = as_paragraph(as_equation('D,\\,\\frac{1}{H}\\left[-bU_{10}^2\\left(\\frac{s_c}{600} \\right)^{-0.5} \\left(C_{Sat} - C_d\\right )\\right]', props = eqsz))) %>% 
   flextable::compose(i = 17, j = 3, value = as_paragraph(as_equation('a', props = eqsz))) %>% 
   flextable::compose(i = 18, j = 3, value = as_paragraph(as_equation('b', props = eqsz))) %>% 
-  flextable::compose(i = 2, j = 4, value = as_paragraph(as_equation('\\text{mg}\\,\\text{L}^{-1}', props = eqsz))) %>% 
+  flextable::compose(i = 2, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-3}', props = eqsz))) %>% 
   flextable::compose(i = 3, j = 4, value = as_paragraph(as_equation('\\degree\\text{C}', props = eqsz))) %>% 
   flextable::compose(i = 4, j = 4, value = as_paragraph(as_equation('\\text{psu}', props = eqsz))) %>% 
-  flextable::compose(i = 5, j = 4, value = as_paragraph(as_equation('\\text{Watts}\\,\\text{m}^{-2}', props = eqsz))) %>% 
+  flextable::compose(i = 5, j = 4, value = as_paragraph(as_equation('\\text{W}\\,\\text{m}^{-2}', props = eqsz))) %>% 
   flextable::compose(i = 6, j = 4, value = as_paragraph(as_equation('\\text{m}\\,\\text{s}^{-1}', props = eqsz))) %>% 
   flextable::compose(i = 7, j = 4, value = as_paragraph(as_equation('\\text{m}', props = eqsz))) %>% 
   flextable::compose(i = 9, j = 4, value = as_paragraph(as_equation('\\text{m}^2\\,\\text{s}^{-2}', props = eqsz))) %>% 
   flextable::compose(i = 11, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-3}', props = eqsz))) %>% 
   flextable::compose(i = 13, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-3}', props = eqsz))) %>% 
-  flextable::compose(i = 14, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1},\\,\\text{mmol}\\,\\text{m}^{-3}\\,\\text{d}^{-1}', props = eqsz))) %>% 
-  flextable::compose(i = 15, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1},\\, \\text{mmol}\\,\\text{m}^{-3}\\,\\text{d}^{-1}', props = eqsz))) %>% 
-  flextable::compose(i = 16, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1},\\, \\text{mmol}\\,\\text{m}^{-3}\\,\\text{d}^{-1}', props = eqsz))) %>% 
-  flextable::compose(i = 17, j = 4, value = as_paragraph(as_equation('\\left(\\text{mmol}\\,\\text{m}^{-3}\\,\\text{d}^{-1}\\right)/\\left(\\text{Watts}\\,\\text{m}^{-2}\\right)', props = eqsz))) %>% 
+  flextable::compose(i = 14, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1}', props = eqsz))) %>% 
+  flextable::compose(i = 15, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1}', props = eqsz))) %>% 
+  flextable::compose(i = 16, j = 4, value = as_paragraph(as_equation('\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1}', props = eqsz))) %>% 
+  flextable::compose(i = 17, j = 4, value = as_paragraph(as_equation('\\left(\\text{mmol}\\,\\text{m}^{-2}\\,\\text{d}^{-1}\\right)/\\left(\\text{W}\\,\\text{m}^{-2}\\right)', props = eqsz))) %>% 
   flextable::compose(i = 18, j = 4, value = as_paragraph(as_equation('\\left(\\text{cm}\\,\\text{hr}^{-1}\\right)/\\left(\\text{m}^2\\,\\text{s}^{-2}\\right)', props = eqsz))) %>%
   flextable::font(part = 'all', fontname = 'Times New Roman') %>% 
   flextable::align(align = 'center', j = 3:4, part = 'all') %>% 
@@ -69,16 +69,17 @@ save(ebaseiotab, file = here('tabs/ebaseiotab.RData'))
 load(file = url('https://github.com/fawda123/BASEmetab_script/raw/master/data/apacmp.RData'))
 
 tosum <- apacmp %>% 
+  filter(var != 'D') %>% 
   mutate(
-    var = factor(var, levels = c('NEM', 'P', 'R', 'D'), 
+    var = factor(var, levels = c('NEM', 'P', 'R'), 
     )
   ) %>% 
-  pivot_wider(names_from = 'typ', values_from = 'val')
+  pivot_wider(names_from = 'typ', values_from = 'val') %>% 
+  na.omit()
 
 grd <- crossing(
   dotyp = unique(tosum$dotyp), 
-  var = levels(tosum$var), 
-  comp = c('Odum v EBASE')
+  var = levels(tosum$var)
   ) %>% 
   mutate(
     corv = NA, 
@@ -87,7 +88,7 @@ grd <- crossing(
     slolo = NA,
     slohi = NA,
     rse = NA,
-    rmse = NA
+    rmsd = NA
   )
 
 for(i in 1:nrow(grd)){
@@ -98,18 +99,15 @@ for(i in 1:nrow(grd)){
                
   dotyp <- grd[i, ]$dotyp
   var <- grd[i, ]$var
-  comp <- grd[i, ]$comp
-  
-  comp <- strsplit(comp, ' v ')[[1]]
   
   # filter data
   tocmp <- tosum %>% 
     filter(dotyp == !!dotyp) %>% 
     filter(var == !!var) %>% 
     rename(
-      yval = !!comp[1], 
-      xval = !!comp[2]
-    )
+      yval = 'Odum', 
+      xval = 'EBASE'
+      )
   
   # get summaries
   corv <- cor.test(tocmp$xval, tocmp$yval)
@@ -124,8 +122,8 @@ for(i in 1:nrow(grd)){
   slolo <- confint(lmmod)[2, 1]
   slohi <- confint(lmmod)[2, 2]
   rse <- round(sigma(lmmod), 2)
-  rmse <- round(modelr::rmse(lmmod, tocmp), 2)
-  
+  rmsd <- as.character(round(sqrt(sum((tocmp$yval - tocmp$xval)^2) / nrow(tocmp)), 2))
+
   # append to output
   grd[i, 'corv'] <- corv
   grd[i, 'int'] <- int
@@ -133,33 +131,26 @@ for(i in 1:nrow(grd)){
   grd[i, 'slolo'] <- slolo
   grd[i, 'slohi'] <- slohi
   grd[i, 'rse'] <- rse
-  grd[i, 'rmse'] <- rmse
+  grd[i, 'rmsd'] <- rmsd
   
 }
 
 totab <- grd %>% 
-  select(dotyp, comp, var, corv, int, slo, rmse) %>% 
+  select(dotyp, var, corv, rmsd) %>% 
+  pivot_longer(corv:rmsd) %>% 
+  unite('name', name, dotyp) %>% 
   mutate(
-    dotyp = factor(dotyp, levels = c('observed', 'detided'), labels = c('Observed', 'Detided')), 
-    comp = factor(comp, levels = c('Odum v EBASE')), 
-    var = factor(var, levels = c('NEM', 'P', 'R', 'D'))
+    name = factor(name, levels = c('corv_observed', 'corv_detided', 'rmsd_observed', 'rmsd_detided')),
+    var = factor(var, levels = c('P', 'R', 'NEM')), 
+    value = gsub('\\*+', '', value)
   ) %>% 
-  arrange(dotyp, comp, var) %>%
-  group_by(dotyp) %>% 
-  mutate(
-    comp = ifelse(duplicated(comp), '', as.character(comp))
-  ) %>% 
+  pivot_wider(names_from = 'name', values_from = 'value') %>% 
+  unite('Corr.', corv_observed, corv_detided, sep = ' / ') %>% 
+  unite('RMSD', rmsd_observed, rmsd_detided, sep = ' / ') %>% 
+  arrange(var) %>%
   rename(
-    `Dissolved Oxygen` = dotyp, 
-    `Comparison` = comp,
-    Estimate = var, 
-    `Corr.` = corv,
-    Intercept = int, 
-    Slope = slo,
-    RMSE = rmse
-  ) %>% 
-  select(-Comparison, -Intercept, -Slope) %>% 
-  flextable::as_grouped_data(groups = c('Dissolved Oxygen'))
+    Parameter = var
+  ) 
 
 apacmptab <- totab %>%
   flextable() %>% 
@@ -167,11 +158,9 @@ apacmptab <- totab %>%
   font(part = 'all', fontname = 'Times New Roman') %>% 
   padding(padding = 1, part = 'all') %>% 
   width(width = 6.5 / ncol_keys(.)) %>% 
-  flextable::compose(part = 'header', j = 'RMSE', value = as_paragraph(as_equation("\\text{RMSE}~(\\text{mmol}~\\text{O}_2/\\text{m}^2/\\text{d})"))) %>% 
-  # flextable::compose(part = 'header', j = 'Intercept', value = as_paragraph(as_equation("\\text{Intercept}~(\\text{mmol}~\\text{O}_2/\\text{m}^2/\\text{d})"))) %>% 
+  flextable::compose(part = 'header', j = 'RMSD', value = as_paragraph(as_equation("\\text{RMSD}~(\\text{mmol}~\\text{O}_2/\\text{m}^2/\\text{d})"))) %>% 
   flextable::compose(part = 'header', j = 'Corr.', value = as_paragraph(as_equation("\\rho"))) %>% 
-  flextable::align(align = 'center', j = 3:ncol_keys(.), part = 'all') %>% 
-  flextable::add_footer_lines(value = '* p < 0.05, ** p < 0.005') %>% 
+  flextable::align(align = 'center', j = 2:ncol_keys(.), part = 'all') %>% 
   font(part = 'footer', fontname = 'Times New Roman') %>% 
   flextable::align(align = 'right', part = 'footer')
 
